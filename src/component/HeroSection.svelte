ea14b630c68ec3ad58f942b3eb954bf847faa4da
Jon <jon@sessionsdev.com>
2022-07-25 09:24:13 -0400

    added hero second with background image

---
+++ src/component/HeroSection.svelte
@@ -0,0 +1,28 @@
+<section class="hero is-fullheight is-fullheight-with-navbar has-background-image">
+    <div class="hero-body">
+        <div class="box mb-auto mt-6 has-background-white">
+
+                <h1 class="title is-1 is-uppercase">
+                    Jon Sessions
+                </h1>
+                <h2 class="subtitle is-3">
+                    Building better web apps.
+                </h2>
+            
+        </div>
+    </div>
+</section>
+
+<style>
+    .hero.has-background-image {
+        background-image: url('/asset/img/main-background.png');
+        background-position: center center;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+    }
+
+    .hero-body {
+        justify-content: left;
+    }
+</style>
--- src/page/MainPage.svelte
+++ src/page/MainPage.svelte
@@ -1,5 +1,7 @@
 <script>
     import NavBar from "../component/NavBar.svelte";
+    import HeroSection from "../component/HeroSection.svelte";
 </script>
 
 <NavBar />
+<HeroSection />
